c5a3a7eabd278de6bbff08d775ab87675a498147
i.shekhurdin <i.shekhurdin@stash.bars-open.ru>
2019-07-09 19:17:52 +0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,96 @@
+extern crate iron;
+#[macro_use] extern crate mime;
+extern crate router;
+extern crate urlencoded;
+
+use iron::prelude::*;
+use iron::status;
+use router::Router;
+use urlencoded::UrlEncodedBody;
+use std::str::FromStr;
+
+fn main() {
+    let mut router = Router::new();
+
+    router.get("/", get_form, "root");
+    router.post("/gcd", post_gcd, "gcd");
+
+    println!("Сервер запущен на http://localhost:3000...");
+    Iron::new(get_form).http("localhost:3000").unwrap();
+}
+
+fn get_form(_request: &mut Request) -> IronResult<Response> {
+    let mut response = Response::new();
+
+    response.set_mut(status::Ok);
+    response.set_mut(mime!(Text/Html; Charset=Utf8));
+    response.set_mut(r#"
+        <title>GCD Calculator</title>
+        <form action="/gcd" method="post">
+        <input type="text" name="n"/>
+        <input type="text" name="n"/>
+        <button type="submit">Compute GCD</button>
+        </form>
+    "#);
+
+    Ok(response)
+}
+
+fn post_gcd(request: &mut Request) -> IronResult<Response> {
+    let mut response = Response::new();
+
+    let form_data = match request.get_ref::<UrlEncodedBody>() {
+        Err(e) => {
+            response.set_mut(status::BadRequest);
+            response.set_mut(format!("Не удалось распознать данные: {:?}\n", e));
+        }
+        Ok(map) => map
+    };
+
+    let unparsed_numbers = match form_data.get("n") {
+        None => {
+            response.set_mut(status::BadRequest);
+            response.set_mut(format!("Форма не содержит параметра 'n'\n"));
+            return Ok(response);
+        }
+        Some(nums) => nums
+    };
+
+    let mut numbers = Vec::new();
+    for unparsed in unparsed_numbers {
+        match u64::from_str(&unparsed) {
+            Err(_) => {
+                response.set_mut(status::BadRequest);
+                response.set_mut(format!("Значение 'n' не является числовым: {:?}\n", unparsed));
+                return Ok(response);
+            }
+            Ok(n) => { numbers.push(n); }
+        }
+    }
+
+    let mut d = numbers[0];
+
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    response.set_mut(status::Ok);
+    response.set_mut(mime!(Text/Html; Charset=Utf8));
+    response.set_mut(
+        format!("Самый большой делитель среди чисел {:?} - <b>{}</b>\n", numbers, d)
+    );
+    Ok(response)
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        } 
+        m = m % n;
+    } 
+    n
+}
